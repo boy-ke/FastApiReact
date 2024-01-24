91bf844eeab0f25744dd655f50ed2f11b3316548
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-01-24 23:43:57 +0300

    updated notes using api

--- main.py
+++ main.py
@@ -41,3 +41,39 @@ def add_note(note: NoteInput):
     finally:
         db.close()
     return new_note
+
+
+@app.get("/notes/{node_id}")
+def read_notes_by_id(note_id: int):
+    db = DBSession()
+    try:
+        note = db.query(models.Note).filter(models.Note.id == note_id).first()
+        if note is None:
+            raise HTTPException(
+                status_code=404, detail={
+                    "status": "Error 404 - Not Found",
+                    "message": f"Note with id {note_id} not found"
+                    })
+    finally:
+        db.close()
+    return note
+
+
+@app.put("/notes/{note_id}")
+def update_note(note_id: int, updated_note: NoteInput):
+    if len(updated_note.title) == 0 and len(updated_note.description) == 0:
+        raise HTTPException(status_code=400, detail={
+            "status": "Error 400 - Bad Request",
+            "message": "Note title and description cannot be empty"
+        })
+    db = DBSession()
+    try:
+        #note_to_update = db.query(models.Note).filter(models.Note.id == note_id).first()
+        note = db.query(models.Note).filter(models.Note.id == note_id).first()
+        note.title = updated_note.title
+        note.description = updated_note.description
+        db.commit()
+        db.refresh(note)
+    finally:
+        db.close()
+    return note
